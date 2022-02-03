b2ee7228768b49ed81d42a6764d56042dc806da0
sabrine loussaief <loussaiefsabrine@gmail.comgit config --global user.email loussaiefsabrine@gmail.com>
2022-02-03 10:13:05 +0100

    My First React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
-import logo from './logo.svg';
+import Container from 'react-bootstrap/Container';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container id="main-container" className="d-grid h-100">
+      <Form id="sign-in-form" className="text-center p-3 w-100">
+        <h1 className="mb-3 fs-3 fw-normal">Please sign in</h1>
+        <Form.Group controlId="sign-in-email-address">
+          <Form.Control type="email" size="lg" placeholder="Email address" autoComplete="username" className="position-relative" />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="sign-in-password">
+          <Form.Control type="password" size="lg" placeholder="Password" autoComplete="current-password" className="position-relative" />
+        </Form.Group>
+        <Form.Group className="d-flex justify-content-center mb-4" controlId="remember-me">
+          <Form.Check label="Remember me" />
+        </Form.Group>
+        <div className="d-grid">
+          <Button variant="primary" size="lg">Sign in</Button>
+        </div>
+        <p className="mt-5 text-muted">&copy; 2022-2023</p>
+      </Form>
+    </Container>
   );
 }
 
